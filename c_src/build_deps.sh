021dcb10f5335932d4113e2299b33306b65f84d9
Gregory Burd <greg@basho.com>
2013-03-12 16:56:00 -0400

    Build from source pulled from a specific branch on GitHub for "basho" related work and integration of WiredTiger rather than the latest .tar.bz2 file they distribute.

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-WT_VSN=1.4.2
+WT_BRANCH=basho
 
 [ `basename $PWD` != "c_src" ] && cd c_src
 
@@ -10,19 +10,24 @@ BASEDIR="$PWD"
 
 case "$1" in
     clean)
-        rm -rf system wiredtiger-$WT_VSN
+        rm -rf system wiredtiger
         ;;
 
     *)
         test -f system/lib/libwiredtiger.a && exit 0
 
-        tar -xjf wiredtiger-$WT_VSN.tar.bz2
-
-        (cd wiredtiger-$WT_VSN/build_posix && \
+	if [ -d wiredtiger/.git ]; then
+	    (cd wiredtiger && \
+		git fetch && \
+		git merge origin/$WT_BRANCH)
+	else
+	    git clone https://github.com/wiredtiger/wiredtiger.git -b $WT_BRANCH && \
+		(cd wiredtiger && ./autogen.sh)
+	fi
+	(cd wiredtiger/build_posix && \
             ../configure --with-pic \
                          --prefix=$BASEDIR/system && \
             make && make install)
-
         ;;
 esac
 
